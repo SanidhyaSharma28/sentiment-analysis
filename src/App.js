f3fa4f33acf55b1f81623802c573b94b6f174242
Sanidhya <sani28sharma@gmail.com>
2023-10-06 22:49:30 +0530

    newly

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import React from 'react'
 import Navbar from './Components/Navbar';
 import Submitter from './Components/Submitter';
-import 'C:/sentiment analysis/watchlist/my-react-app/src/bgimg.css'
 
 function App() {
   return (
